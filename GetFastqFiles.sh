77cf9db3758a66516be0ed96da33f3100aa377a0
zlewis75 <zacharyaustinlewis@gmail.com>
2022-08-26 13:32:29 -0400

    fix if then loop

--- GetFastqFiles.sh
+++ GetFastqFiles.sh
@@ -3,12 +3,13 @@
 #check for required command line argument
 
 if [ -z "$1" ] then
-	do
 		echo "Include the path to your accession file in the command line call. eg. sh GetFastqFiles.sh Path/To/Your/AccessionFile.txt"
 		exit
 
 	else
 chmod 755 curlFastq.sh
+done
+
 
 #iterates through a directory of directories, each containing a metadata file produced by ffq --ftp. replace /scratch/zlewis/SRAtest/ with the path to your own directory.
 
