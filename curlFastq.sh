bf7bd79f46ceda3f555dde1b9f9424824fb3b0af
zlewis75 <zacharyaustinlewis@gmail.com>
2022-08-26 12:49:22 -0400

    Create curlFastq.sh

---
+++ curlFastq.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -ex
+
+#the SRR accession will be stored in variable $1, passed from GetFastqFiles.sh
+#cd into accession directory
+cd ./FastqFiles/${1}
+
+#this command extracts the url tag from the .json file, then extract the url, then passes that to xargs, which then passes each url to curl iteratively (-n 1)
+grep -Eo '"url": "[^"]*"' ./FastqFiles/${1}.json | grep -o '"[^"]*"$' | xargs -n 1 curl -C - -O
