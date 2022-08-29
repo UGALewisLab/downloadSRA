2fec9fa5258fd5df5a565a2c65ef548207701fe8
zlewis75 <zacharyaustinlewis@gmail.com>
2022-08-29 12:35:11 -0400

    added second curl call as backup. added quiet mode with error reporting only

--- curlFastq.sh
+++ curlFastq.sh
@@ -5,4 +5,8 @@
 cd ./FastqFiles/${1}
 
 #this command extracts the url tag from the .json file, then extract the url, then passes that to xargs, which then passes each url to curl iteratively (-n 1)
-grep -Eo '"url": "[^"]*"' ./${1}.json | grep -o '"[^"]*"$' | xargs -n 1 curl -C - -O
+grep -Eo '"url": "[^"]*"' ./${1}.json | grep -o '"[^"]*"$' | xargs -n 1 curl -sS -C - -O
+
+grep -Eo '"url": "[^"]*"' ./${1}.json | grep -o '"[^"]*"$' | xargs -n 1 curl -sS-C - -O
+
+exit
