f05cabe17d88ad13c7c4dcfc13fa3f0578d74c84
zlewis75 <zacharyaustinlewis@gmail.com>
2022-08-30 15:24:14 -0400

    added loop to check exit code from grep/curl call. If exit is not '0', run grep/curl again. If exit gives an error code a second time, echo exit code and exit

--- curlFastq.sh
+++ curlFastq.sh
@@ -7,6 +7,15 @@ cd ./FastqFiles/${1}
 #this command extracts the url tag from the .json file, then extract the url, then passes that to xargs, which then passes each url to curl iteratively (-n 1)
 grep -Eo '"url": "[^"]*"' ./${1}.json | grep -o '"[^"]*"$' | xargs -n 1 curl  -sS -C - -O
 
-grep -Eo '"url": "[^"]*"' ./${1}.json | grep -o '"[^"]*"$' | xargs -n 1 curl  -sS -C - -O
+res=$?
+if test "$res" != "0"; then
+  echo "${1}, exit code ${res}: running grep/curl call again"
+
+  grep -Eo '"url": "[^"]*"' ./${1}.json | grep -o '"[^"]*"$' | xargs -n 1 curl  -sS -C - -O
+
+fi
+
+echo "${1}, exit code ${?}"
+#grep -Eo '"url": "[^"]*"' ./${1}.json | grep -o '"[^"]*"$' | xargs -n 1 curl  -sS -C - -O
 
 exit
